3d2f2088f1b03d5c7d38a0998923d37f56b4f144
Ordon <ordon1313@gmail.com>
2015-05-14 00:55:26 +0200

    Added GameOfLifeSpec

--- src/test/java/com/helloordon/hellospock/GameOfLifeSpec.groovy
+++ src/test/java/com/helloordon/hellospock/GameOfLifeSpec.groovy
@@ -3,4 +3,8 @@ package com.helloordon.hellospock
 import spock.lang.Specification
 
 final class GameOfLifeSpec extends Specification {
+    def "Test example"() {
+        expect:
+        true
+    }
 }
