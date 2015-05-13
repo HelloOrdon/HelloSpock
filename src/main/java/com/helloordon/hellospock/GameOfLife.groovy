680ab31cac5b842a7db6abea676620507b252787
Ordon <ordon1313@gmail.com>
2015-05-14 01:04:30 +0200

    Game create with life should be alive

--- src/main/java/com/helloordon/hellospock/GameOfLife.groovy
+++ src/main/java/com/helloordon/hellospock/GameOfLife.groovy
@@ -4,7 +4,13 @@ import groovy.transform.CompileStatic
 
 @CompileStatic
 final class GameOfLife {
+    private List<List<Integer>> lives
+
+    GameOfLife(List<List<Integer>> lives) {
+        this.lives = lives
+    }
+
     boolean isAlive(int x, int y) {
-        return false
+        return !lives.empty
     }
 }
--- src/test/java/com/helloordon/hellospock/GameOfLifeSpec.groovy
+++ src/test/java/com/helloordon/hellospock/GameOfLifeSpec.groovy
@@ -3,15 +3,20 @@ package com.helloordon.hellospock
 import spock.lang.Specification
 
 final class GameOfLifeSpec extends Specification {
-    def "Should create game"() {
-        expect:
-        new GameOfLife()
-    }
 
     def "Game should be dead on start"() {
         given:
-        def game = new GameOfLife()
+        def game = new GameOfLife([])
+
         expect:
-        game.isAlive(0, 0) == false
+        !game.isAlive(0, 0)
+    }
+
+    def "Game create with life should be alive"() {
+        given:
+        def game = new GameOfLife([[0,0]])
+
+        expect:
+        game.isAlive(0, 0)
     }
 }
